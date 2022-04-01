6cf9ae055ab05e4fa2a2bd8a829d81e9bae9e18d
vvo806 <vvo22@students.bmchs.org>
2022-04-01 08:05:17 -0500

    one more thing to do

--- ap_project.py
+++ ap_project.py
@@ -1,60 +1,74 @@
 "Making rock-paper-scissors project"
 
 import random
-from re import U
-from tracemalloc import stop
 import collections
 import time
 
-def rock(): #respond with results when computer chooses rock
-    if u_choice == 'R':
-        round.append('tie')
-    elif u_choice == 'P':
-        round.append('user')
-    else:
-        round.append('computer')
-def paper(): #respond with results when computer chooses paper
-    if u_choice == 'R':
-        round.append('computer')
-    elif u_choice == 'P':
-        round.append('tie')
-    else:
-        round.append('user')
-def scissor(): #respond with results when computer chooses scissor
-    if u_choice == 'R':
-        round.append('user')
-    elif u_choice == 'P':
-        round.append('computer')
-    else:
-        round.append('tie')
+# Defines the winner into a list after comparing user's and computer's weapon
+def results(x):
+    # Respond with results when computer chooses rock
+    if x == 'Rock': 
+        if u_choice == 'R':
+            round.append('tie')
+        elif u_choice == 'P':
+            round.append('user')
+        else:
+            round.append('computer')
 
+    # Respond with results when computer chooses paper
+    if x == 'Paper': 
+        if u_choice == 'R':
+            round.append('computer')
+        elif u_choice == 'P':
+            round.append('tie')
+        else:
+            round.append('user')
+
+    # Respond with results when computer chooses scissor
+    if x == 'Scissor': 
+        if u_choice == 'R':
+            round.append('user')
+        elif u_choice == 'P':
+            round.append('computer')
+        else:
+            round.append('tie')
+
+# List of winners per round
 round = []
 
-print("Welcome to The Game of Rock, Papers, Scissoers!")
+# Introductions
+print("Welcome to The Game of Rock, Papers, Scissors!")
 print()
-print("You will be playing 3 games with the computer. One with most wins is the winner.")
-time.sleep(4)
+print("You will be playing 3 rounds with the computer. One with most wins is the winner.")
+time.sleep(3)
 print()
 name = input('Name: ')
 c_name = input("Computer's name: ")
 
+# To count the rounds of the game
 count = 1
+
+# Playing 3 rounds
+weapon = ['Rock', 'Paper', 'Scissor']
 for i in range(3):
-    weapon = ['Rock', 'Paper', 'Scissor']
     u_choice = input(f'{count}. {name}, what do you want to choose? Rock, paper, or scissor? (R,P,S): ')
 
     computer = random.choice(weapon)
 
-    if computer == 'Rock':
-        rock()
+    rock = 'Rock'
+    paper = 'Paper'
+    scissor = 'Scissor'
+
+    if computer == rock:
+        results('Rock')
         count += 1
 
-    elif computer == 'Paper':
-        paper()
+    elif computer == paper:
+        results('Paper')
         count += 1
 
     else:
-        scissor()
+        results(scissor)
         count += 1
 
     print(f'{c_name} choose {computer}')
@@ -62,10 +76,20 @@ for i in range(3):
 
 print(round)
 
-#Finding the who won 2/3 games to determine winner
-most = [item for item, count in collections.Counter(round).items() if count > 1] #finds duplicate in list
+# Finding who won at least 2/3 games to determine winner
+# Credit for Line 75: 
+for item in round:
+    u = 'user'
+    c = 'computer'
+    t = 'tie'
+    print(round)
+    #if int(item) in round > 1:
+     #   print(item)
+
+most = [item for item, count in collections.Counter(round).items() if count > 1] # Finds duplicate in list
 print()
 
+# Prints out winner's name
 if most == ['user']:
     print(f'{name} wins!')
 
